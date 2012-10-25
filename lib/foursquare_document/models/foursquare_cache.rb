d6f1802a05b65f1466640c81bf0a279a48bb41fa
Niels Vandekeybus <nvdk@arguslabs.be>
2012-10-25 13:27:58 +0200

    include contact and url

--- lib/foursquare_document/models/foursquare_cache.rb
+++ lib/foursquare_document/models/foursquare_cache.rb
@@ -7,8 +7,10 @@ class FoursquareCache
   field :categories,  :type => Array
   field :location,    :type => Hash
   field :id,          :type => Moped::BSON::ObjectId
+  field :contact,     :type => Hash
+  field :url,         :type => String
   
-  attr_accessible :name,:description,:categories,:location,:id
+  attr_accessible :name,:description,:categories,:location,:id,:url,:contact
   
   embedded_in :fs_doc,:polymorphic => true
 end
