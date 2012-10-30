345fced3f99f0282b508fcb9297924011f30090e
Niels V <nvdk@arguslabs.be>
2012-10-30 12:21:39 +0100

    added foursquare_photo document

---
+++ lib/foursquare_document/models/foursquare_photo.rb
@@ -0,0 +1,13 @@
+# (c) 2012 Wep'IT bvba
+class FoursquarePhoto
+  include Mongoid::Document
+  include Mongoid::Timestamps::Updated
+  
+  field :id,        :type => String
+  field :createdAt, :type => DateTime
+  field :prefix,    :type => String
+  field :suffix,    :type => String
+  field :source,    :type => Hash
+  
+  embedded_in :foursquare_cache
+end
