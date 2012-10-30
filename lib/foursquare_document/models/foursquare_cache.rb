6b54c63dbbb530d14016d14a436b356c911980d0
Niels V <nvdk@arguslabs.be>
2012-10-30 12:21:57 +0100

    add foursquare_photo embed

--- lib/foursquare_document/models/foursquare_cache.rb
+++ lib/foursquare_document/models/foursquare_cache.rb
@@ -9,8 +9,17 @@ class FoursquareCache
   field :id,          :type => Moped::BSON::ObjectId
   field :contact,     :type => Hash
   field :url,         :type => String
-  
   attr_accessible :name,:description,:categories,:location,:id,:url,:contact
   
   embedded_in :fs_doc,:polymorphic => true
+  embeds_one :foursquare_photo, :as => :photo
+  
+  
+  def add_foursquare_photo
+    client = FoursquareDocument::Config::get_client
+    raw_info = client.venue_photos(self.id)
+    if raw_info.has_key?('items') && raw_info['items'].count > 0
+      self.photo = FoursquarePhoto.new(raw_info['items'][0])
+    end
+  end
 end
