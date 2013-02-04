261081c35ff0e39293c2536704654f44eb61483d
Tj <tj@arguslabs.be>
2013-02-04 17:00:42 +0100

    do not require extra options

--- lib/foursquare_document/config.rb
+++ lib/foursquare_document/config.rb
@@ -8,7 +8,7 @@ module FoursquareDocument
       yield FS_CONFIG
     end
 
-    def self.get_client(extra_options)
+    def self.get_client(extra_options={})
       options = {:client_id => FS_CONFIG.client_id, :client_secret => FS_CONFIG.client_secret,:api_version => "20121030"}.merge!(extra_options)
       @client = Foursquare2::Client.new(options)
     end
